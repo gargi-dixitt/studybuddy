8fbe3b1ab2630e3e919e69b1a534864d6dba4d97
Gargi Dixit <gargidixit2007@gmail.com>
2026-03-23 19:46:29 +0530

    Intial Studybuddy prject

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import TaskForm from './components/TaskForm';
+import TaskCard from './components/TaskCard';
+import Dashboard from './pages/Dashboard';
+import Timer from './components/Timer';
+
+
 
 function App() {
+    const [tasks, setTasks] = useState([])
+
+  const addTask = (task) => {
+    setTasks([...tasks, { ...task, completed: false }])
+  }
+
+
+  const deleteTask = (index) => {
+    const updated = tasks.filter((_, i) => i !== index)
+    setTasks(updated)
+  }
+
+  const completeTask = (index) => {
+    const updated = tasks.map((t, i) => 
+      i === index ? { ...t, completed: true } : t
+    )
+    setTasks(updated)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Dashboard tasks={tasks} />
+      <Timer />
+      <TaskForm addTask={addTask} />
+      <div className="container mt-4">
+        {tasks.map((task, index) => (
+          <TaskCard
+            key={index}
+            task={task}
+            deleteTask={() => deleteTask(index)}
+            completeTask={() => completeTask(index)}
+          />
+        ))}
+      </div>
     </div>
   );
 }
